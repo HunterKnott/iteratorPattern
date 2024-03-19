93efb4a0834e2a45ce53df29c559445b291c2a8c
Hunter Knott <hunterknott00@gmail.com>
2024-03-19 12:27:10 -0600

    Initial Commit

---
+++ iteratorPattern/src/controller/Runner.java
@@ -0,0 +1,10 @@
+// Hunter Knott, CS 3450, Utah Valley University
+
+package controller;
+
+public class Runner {
+	public static void main(String[] args) {
+		Controller app = new Controller();
+		app.run();
+	}
+}
